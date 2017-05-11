690e8c953302d6bc14c37f431780ab0bcc6b13d4
wybrenoppedijk <wybren13@gmail.com>
2017-05-11 17:14:18 +0200

    Added constructor still in progress.

--- app/src/main/java/com/example/duif/controller/TweetListAdapter.java
+++ app/src/main/java/com/example/duif/controller/TweetListAdapter.java
@@ -3,12 +3,42 @@ package com.example.duif.controller;
 import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.duif.R;
 import com.example.duif.model.Tweet;
 
-public class TweetListAdapter extends ArrayAdapter{
+import java.util.zip.Inflater;
+
+public class TweetListAdapter extends ArrayAdapter<Tweet>{
     public TweetListAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull Tweet[] tweets) {
-        super(context, resource, tweets);
+        super(context, R.layout.list_item_tweet, tweets);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View view = inflater.inflate(R.layout.list_item_tweet, parent, false);
+        Tweet tweet = getItem(position);
+
+        TextView name = (TextView) view.findViewById(R.id.name);
+        TextView screeName = (TextView) view.findViewById(R.id.screen_name);
+        TextView text = (TextView) view.findViewById(R.id.text);
+        TextView date = (TextView) view.findViewById(R.id.date);
+        TextView retweet = (TextView) view.findViewById(R.id.retweets);
+        TextView favourites = (TextView) view.findViewById(R.id.favourites);
+
+        ImageView icon = (ImageView) view.findViewById(R.id.icon);
+
+
+
+        return view;
     }
 }
